7d185c3373ac087fe3a505bf7482ebe3d69bb8e7
rick0335 <43598751+rick0335@users.noreply.github.com>
2019-11-05 00:04:23 +0100

    Template til Ustabilkode api, service lag

---
+++ UstabilKodeMVC/UstabilKodeMVC/Services/UstabilkodeAPI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UstabilKodeMVC.Services
+{
+    public static class UstabilkodeAPI
+    {
+        private const string apiurl = "https://ustabilkode-api.azurewebsites.com/api";
+
+        #region Products
+        public static void GetProducts()
+        {
+
+        }
+
+        public static void GetProduct(int id)
+        {
+
+        }
+
+        public static void CreateProduct()
+        {
+
+        }
+
+        public static void UpdateProduct()
+        {
+
+        }
+
+        public static void DeleteProduct()
+        {
+
+        }
+        #endregion
+
+        #region Posts
+        public static void GetPosts()
+        {
+
+        }
+
+        public static void GetPost(int id)
+        {
+
+        }
+
+        public static void CreatePost()
+        {
+
+        }
+
+        public static void UpdatePost()
+        {
+
+        }
+
+        public static void DeletePost()
+        {
+
+        }
+        #endregion
+
+        #region Comments
+        public static void GetComments()
+        {
+
+        }
+
+        public static void GetComment(int id)
+        {
+
+        }
+
+        public static void CreateComment()
+        {
+
+        }
+
+        public static void UpdateComment()
+        {
+
+        }
+
+        public static void DeleteComment()
+        {
+
+        }
+        #endregion
+    }
+}
