fb0dd1f72b601327d5e99374b2cd6b00cdb3ca90
Muslim Al-Ali <musl0074@edu.eal.dk>
2019-11-04 19:58:49 +0100

    Implementing API

---
+++ UstabilkodeApi/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UstabilkodeApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UstabilkodeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly UstabilkodeContext _context;
+
+        public ProductController(UstabilkodeContext context)
+        {
+            _context = context;
+
+            if (_context.Products.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Products.Add(new Product { Name = "Item1", Details = "blala", Price = 500 });
+                _context.Products.Add(new Product { Name = "Item1", Details = "blala", Price = 500 });
+                _context.Products.Add(new Product { Name = "Item1", Details = "blala", Price = 500 });
+                _context.Products.Add(new Product { Name = "Item1", Details = "blala", Price = 500 });
+
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // POST: api/product
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ UstabilkodeApi/Models/UstabilkodeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UstabilkodeApi.Models
+{
+    public class UstabilkodeContext : DbContext
+    {
+        public UstabilkodeContext(DbContextOptions<UstabilkodeContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
