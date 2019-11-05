6633615f14618555ef6b83b9bccb2b375986966d
Frank Nordahl Andersen <fran2238@edu.eal.dk>
2019-11-05 12:03:34 +0100

    Service Product api

--- UstabilKodeMVC/UstabilKodeMVC/Services/UstabilkodeAPI.cs
+++ UstabilKodeMVC/UstabilKodeMVC/Services/UstabilkodeAPI.cs
@@ -2,37 +2,101 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using UstabilKodeMVC.Models;
+using Newtonsoft.Json;
 
 namespace UstabilKodeMVC.Services
 {
     public static class UstabilkodeAPI
     {
-        private const string apiurl = "https://ustabilkode-api.azurewebsites.com/api";
+        private const string apiurl = "https://ustabilkode-api.azurewebsites.net/api";
+        private static HttpClient client = new HttpClient();
 
         #region Products
-        public static void GetProducts()
+        public async static Task<List<Product>> GetProducts()
         {
+            string getProductsUrl = "https://ustabilkode-api.azurewebsites.net/api/Product";
+
+            //  var request = (HttpWebRequest)WebRequest.Create(getProductsUrl);
+            //var response = (HttpWebResponse)request.GetResponse();
+            HttpResponseMessage response =await client.GetAsync(getProductsUrl);
+            string productsString=await response.Content.ReadAsStringAsync();
+
+           // string productsString = new StreamReader(response.Content).ReadToEnd();
+
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(productsString);
+
+            return products;
+        }
+
+        public async static Task<Product> GetProduct(int id)
+        {
+            string getProductUrl = "https://ustabilkode-api.azurewebsites.net/api/Product/" + id;
+
+            //  var request = (HttpWebRequest)WebRequest.Create(getProductUrl);
+            //var response = (HttpWebResponse)request.GetResponse();
+            string productString = await client.GetStringAsync(getProductUrl);
+           // string productString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            Product product = JsonConvert.DeserializeObject<Product>(productString);
+
+            return product;
+        }
+
+        public async static Task<HttpResponseMessage> CreateProduct(string name, string details, double price)
+        {
+            string postCreateProductUrl = "https://ustabilkode-api.azurewebsites.net/api/product";
+            
+
+            Product product = new Product()
+            {
+                ProductName = name,
+                Details = details,
+                Price = price
+            };
+            string json = JsonConvert.SerializeObject(product);
+            StringContent message = new StringContent(json);
+
+            //   var request = (HttpWebRequest)WebRequest.Create(postCreateProductUrl);
+
+           return await client.PostAsync(postCreateProductUrl,message);
 
         }
 
-        public static void GetProduct(int id)
+        public async static Task<HttpResponseMessage> UpdateProduct(int id,string name,string details,double price)
         {
+            string postUpdateProductUrl = "https://ustabilkode-api.azurewebsites.net/api/product/" + id;
 
+            Product product = new Product()
+            {
+                ID = id,
+                ProductName = name,
+                Details = details,
+                Price = price
+            };
+
+            string json = JsonConvert.SerializeObject(product);
+            StringContent message = new StringContent(json);
+
+            return await client.PutAsync(postUpdateProductUrl, message);
         }
 
-        public static void CreateProduct()
+        public async static Task<HttpResponseMessage> DeleteProduct(int id)
         {
+            string deleteProductURL = "https://ustabilkode-api.azurewebsites.net/api/product/" + id;
 
-        }
+            Product product = new Product
+            {
+                ID = id
+            };
 
-        public static void UpdateProduct()
-        {
+            string json = JsonConvert.SerializeObject(product);
+            StringContent message = new StringContent(json);
 
-        }
-
-        public static void DeleteProduct()
-        {
-
+            return await client.DeleteAsync(deleteProductURL);
         }
         #endregion
 
