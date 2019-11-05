2d570c5b2e4c5c220a81af6b74de8c91dc2c9095
Rick0335 <Rick0335@edu.eal.dk>
2019-11-05 10:45:35 +0100

    Tilføjet Navigation properties til Posts & Comments

--- UstabilkodeApi/Models/Post.cs
+++ UstabilkodeApi/Models/Post.cs
@@ -7,8 +7,13 @@ namespace UstabilkodeApi.Data
 {
     public class Post
     {
+        // Keys
         public int ID { get; set; }
 
+        // Navigation properties
+        public ICollection<Comment> Comments { get; set; }
+
+        // Data-Properties
         public string Title { get; set; }
         public string Content { get; set; }
     }
