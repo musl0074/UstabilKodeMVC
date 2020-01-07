111caa50f2763e467e59f40d996789445af849bd
Muslim Al-Ali <musl0074@edu.eal.dk>
2020-01-07 23:51:39 +0100

    Outcomment Subcription

--- UstabilKodeMVC/UstabilKodeMVC/Services/UstabilkodeAPI/SubscriptionEndpoints.cs
+++ UstabilKodeMVC/UstabilKodeMVC/Services/UstabilkodeAPI/SubscriptionEndpoints.cs
@@ -1,80 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using System.IO;
-using UstabilKodeMVC.Models;
-using Newtonsoft.Json;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Net;
+//using System.Net.Http;
+//using System.IO;
+//using UstabilKodeMVC.Models;
+//using Newtonsoft.Json;
+//using System.Text;
 
-namespace UstabilKodeMVC.Services.UstabilkodeAPI
-{
-    public class SubscriptionEndpoints
-    {
-        public async static Task<List<Subscription>> GetSubscriptions(string userId)
-        {
-            string getUrl = APISettings.APIUrl + "/Subscription/" + userId;
-
-
-            HttpResponseMessage response = await APISettings.Client.GetAsync(getUrl);
-            string subscriptionsString = await response.Content.ReadAsStringAsync();
+//namespace UstabilKodeMVC.Services.UstabilkodeAPI
+//{
+//    public class SubscriptionEndpoints
+//    {
+//        public async static Task<List<Subscription>> GetSubscriptions(string userId)
+//        {
+//            string getUrl = APISettings.APIUrl + "/Subscription/" + userId;
 
 
-            List<Subscription> subscriptions = JsonConvert.DeserializeObject<List<Subscription>>(subscriptionsString);
+//            HttpResponseMessage response = await APISettings.Client.GetAsync(getUrl);
+//            string subscriptionsString = await response.Content.ReadAsStringAsync();
 
-            return subscriptions;
-        }
 
-        public async static Task<Subscription> GetSubscription(int id)
-        {
-            string getUrl = APISettings.APIUrl + "/Subscription/" + id;
+//            List<Subscription> subscriptions = JsonConvert.DeserializeObject<List<Subscription>>(subscriptionsString);
 
-            HttpResponseMessage response = await APISettings.Client.GetAsync(getUrl);
-            string subscriptionString = await response.Content.ReadAsStringAsync();
+//            return subscriptions;
+//        }
 
-            Subscription subscription = JsonConvert.DeserializeObject<Subscription>(subscriptionString);
+//        public async static Task<Subscription> GetSubscription(int id)
+//        {
+//            string getUrl = APISettings.APIUrl + "/Subscription/" + id;
 
-            return subscription;
-        }
+//            HttpResponseMessage response = await APISettings.Client.GetAsync(getUrl);
+//            string subscriptionString = await response.Content.ReadAsStringAsync();
 
-        public async static Task<HttpResponseMessage> CreateSubscription(int productId, string userId, DateTime expirationDate)
-        {
-            string postUrl = APISettings.APIUrl + "/Subscription";
+//            Subscription subscription = JsonConvert.DeserializeObject<Subscription>(subscriptionString);
 
-            Subscription subscription = new Subscription()
-            {
-                UserID = userId,
-                ProductID = productId,
-                ExpirationDate = expirationDate
-            };
+//            return subscription;
+//        }
 
-            string json = JsonConvert.SerializeObject(subscription);
-            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
+//        public async static Task<HttpResponseMessage> CreateSubscription(int productId, string userId, DateTime expirationDate)
+//        {
+//            string postUrl = APISettings.APIUrl + "/Subscription";
 
-            var response = await APISettings.Client.PostAsync(postUrl, body);
+//            Subscription subscription = new Subscription()
+//            {
+//                UserID = userId,
+//                ProductID = productId,
+//                ExpirationDate = expirationDate
+//            };
 
-            return response;
-        }
+//            string json = JsonConvert.SerializeObject(subscription);
+//            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
 
-        public async static Task<HttpResponseMessage> RenewSubscription(Subscription subscription)
-        {
-            string postUrl = APISettings.APIUrl + "/Subscription/Renew";
+//            var response = await APISettings.Client.PostAsync(postUrl, body);
 
-            string json = JsonConvert.SerializeObject(subscription);
-            StringContent message = new StringContent(json, Encoding.UTF8, "application/json");
+//            return response;
+//        }
 
-            return await APISettings.Client.PutAsync(postUrl, message);
-        }
+//        public async static Task<HttpResponseMessage> RenewSubscription(Subscription subscription)
+//        {
+//            string postUrl = APISettings.APIUrl + "/Subscription/Renew";
 
-        public async static Task<HttpResponseMessage> DeleteSubscription(int id)
-        {
-            string deleteURL = APISettings.APIUrl + "/Subscription/" + id;
+//            string json = JsonConvert.SerializeObject(subscription);
+//            StringContent message = new StringContent(json, Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await APISettings.Client.DeleteAsync(deleteURL);
+//            return await APISettings.Client.PutAsync(postUrl, message);
+//        }
 
-            return response;
-        }
-    }
-}
+//        public async static Task<HttpResponseMessage> DeleteSubscription(int id)
+//        {
+//            string deleteURL = APISettings.APIUrl + "/Subscription/" + id;
+
+//            HttpResponseMessage response = await APISettings.Client.DeleteAsync(deleteURL);
+
+//            return response;
+//        }
+//    }
+//}
